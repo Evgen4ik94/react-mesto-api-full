d2190e6b115c573197ce2de9eb6059accd5c2c93
Evgeniy Milyakov <milyakove@gmail.com>
2022-11-15 21:32:58 +0300

    check frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,4 @@ import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./components/App";
 
-
 createRoot(document.getElementById("page")).render(<React.StrictMode><App /></React.StrictMode>);
