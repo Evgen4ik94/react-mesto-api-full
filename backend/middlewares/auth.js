ed85e3acb600bd9ed178ff2fbd394f98ee07b44a
Evgeniy Milyakov <milyakove@gmail.com>
2022-11-16 21:25:54 +0300

    Changed Cors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,20 +1,17 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация');
-  }
-  const token = authorization.replace('Bearer ', '');
-  let payload;
+    return next(new AuthError('Необходима авторизация'));
+  } let token = authorization.replace('Bearer ', '');
   try {
-    payload = jwt.verify(token, 'dev-secret-token');
+    token = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new AuthError('Необходима авторизация');
-  }
-
-  req.user = payload;
-  next();
+    return next(new AuthError('Необходима авторизация'));
+  } req.user = token;
+  return next();
 };
