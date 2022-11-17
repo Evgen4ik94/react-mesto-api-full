4317155aee638c5c8f6d8dbefc940fa9b678230b
Evgeniy Milyakov <milyakove@gmail.com>
2022-11-17 22:17:21 +0300

    reload frontend

--- frontend/src/utils/ApiAuth.js
+++ frontend/src/utils/ApiAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co/';
+export const BASE_URL = 'https://api.mesto-exo.nomoredomains.icu';
 
 const request = ({ url, method = 'POST', token, body }) => {
   const config = {
