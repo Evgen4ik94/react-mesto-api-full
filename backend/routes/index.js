956bf960dc778111de3b7ae5a985dced8c17662d
Evgeniy Milyakov <milyakove@gmail.com>
2022-11-22 21:50:31 +0300

    fixed routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,6 +12,7 @@ const routes = express.Router();
 
 routes.all('*', express.json());
 
+
 routes.post(
   '/signup',
   validateCreateUser,
@@ -23,9 +24,10 @@ routes.post(
   validateLogin,
   login,
 );
+routes.use(auth);
 
-routes.use('/users', auth, userRouter);
-routes.use('/cards', auth, cardRouter);
+routes.use('/users', userRouter);
+routes.use('/cards', cardRouter);
 
 routes.all('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
