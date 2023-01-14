c67cb69476db1ef01f1c8f2cbb495b4ddd4c7e48
Evgeniy Milyakov <milyakove@gmail.com>
2023-01-14 16:05:47 +0300

    fixed after second review

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'new-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new IncorrectTokenError('Необходима авторизация'));
   }
