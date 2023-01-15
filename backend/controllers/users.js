6287ebeb0483fe3eb97d2356ace11433719f071a
Evgeniy Milyakov <milyakove@gmail.com>
2023-01-15 15:53:36 +0300

    fixed jwt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -75,8 +75,6 @@ module.exports.login = (req, res, next) => {
     })
     .catch((err) => next(err));
 };
-NODE_ENV=production
-JWT_SECRET=eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b
 module.exports.editUser = (req, res, next) => {
   const { name, about } = req.body;
 
