768d141896742f5f59e1a78f3adefc0358146a8b
Evgeniy Milyakov <milyakove@gmail.com>
2023-01-15 15:08:38 +0300

    fixed jwt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -70,12 +70,13 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51beb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => next(err));
 };
-
+NODE_ENV=production
+JWT_SECRET=eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b
 module.exports.editUser = (req, res, next) => {
   const { name, about } = req.body;
 
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new IncorrectTokenError('Необходима авторизация'));
   }
